82aa4a64e7abde9e1088591cda7c4086386e6534
etic86 <61601604+etic86@users.noreply.github.com>
2021-07-26 22:12:54 -0500

    Add files via upload
    últimos cambios

---
+++ admin_sociometria.php
@@ -0,0 +1,97 @@
+<?php
+session_start();
+if(empty( $_SESSION ['user'])){
+	header('Location:index.php');
+	}
+//Se inicializa la sesión, y a su vez se preparan el buffer y la conexión con la base de datos
+header('Content-Type: text/html; charset=utf-8');
+ob_start();
+?>
+
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Matchpeople - Estudios de sociometría</title>
+<?php include("header.php");?>
+<style>.titulosec {width: 88%;}</style>
+<div id="contenido">
+<?php include("menus/menu_vacantes.php");?>
+  <div class="titulosec" >
+  Estudios de sociometría
+	<?php include("menus/menu_sociometria.php");?>
+  </div>
+  <div class="centar" style="width: 90% !important;">
+    <?php
+		//Conexion a la db
+		include("clavesbd.php");
+		// Crea la conexión
+		$con = mysqli_connect($servername, $username, $password, $dbname);
+		mysqli_set_charset($con,"utf8");
+
+		// Checa que la conexión se haya establecido correctamente
+		if (mysqli_connect_errno()) {
+		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		}
+
+	 $preSql = "SELECT COUNT(IdResEncuesta) AS totalEnc FROM resencuesta_sociometria WHERE IdEmpresa = 1";
+	 $posSql = "SELECT COUNT(IdResEncuesta) AS finishedEnc FROM resencuesta_sociometria WHERE IdEmpresa = 1 AND IdEstatus = 3;";
+	 $res1 = mysqli_query($con, $preSql);
+	 if (mysqli_num_rows($res1) > 0) {
+		while($row = mysqli_fetch_assoc($res1)) {
+			$totalEnc = $row['totalEnc'];
+		}
+	 }
+
+	 $res2 = mysqli_query($con, $posSql);
+	 if (mysqli_num_rows($res2) > 0) {
+		while($row = mysqli_fetch_assoc($res2)) {
+			$finishedEnc = $row['finishedEnc'];
+		}
+	 }
+
+	 echo '<script>console.log('.$totalEnc.')</script>';
+	 echo '<script>console.log('.$finishedEnc.')</script>';
+
+	 if($finishedEnc == 0) {
+	 	$percent = 0;
+	 } else {
+	 	$percent = floor(($finishedEnc * 100)/$totalEnc);
+	 }
+
+	 $sql = "SELECT encuesta_sociometria.IdEncuesta, empresas_sociometria.Nombre, encuesta_sociometria.Titulo,
+	 encuesta_sociometria.Fecha,encuesta_sociometria.porcentaje, encuesta_sociometria.IdEmpresa, encuesta_sociometria.papeleria, encuesta_sociometria.estatusId
+	 FROM encuesta_sociometria
+	 INNER JOIN empresas_sociometria ON encuesta_sociometria.IdEmpresa=empresas_sociometria.IdEmpresa where encuesta_sociometria.estatusId != 3 AND encuesta_sociometria.papeleria != 1 ";
+  			$result = mysqli_query($con, $sql);
+  			if (mysqli_num_rows($result) > 0) {
+  				// output data of each row
+  				while($row = mysqli_fetch_assoc($result)) {
+  						$IdEmpresa = $row['IdEmpresa'];
+						$Titulo = $row['Titulo'];
+						$Titulo= strtoupper($Titulo);
+						$Fecha = $row['Fecha'];
+						$newDate = date("Y", strtotime($Fecha));
+						$Empresa = $row["Nombre"];
+						$Porcentaje = $row["porcentaje"];
+						$estatusId = $row['estatusId'];
+  				echo "<form class='box_vacante' action='editar_sociometria.php?id=".$row['IdEncuesta']."' enctype='multipart/form-data' method='POST'>";?>
+	   				<div class='col1 setenta'>
+		   				<strong>Nombre de estudio:</strong> <span class='h2'><?php echo"$Titulo"; ?></span><br>
+							<strong>Año:</strong> <?php echo"$newDate "; ?>
+							<strong class='izquierdo'>Empresa: </strong><?php echo"$Empresa"; ?>
+							<strong class='izquierdo'>Estatus: </strong><?php if($IdEmpresa == 1) {echo"$percent %";} else {echo"$Porcentaje %";} ?>
+							<strong class='izquierdo'>Estado: </strong>
+						 	<?php if ($estatusId == "2") { $estatus ="Borrador";} else if($estatusId == "1"){$estatus ="Activa";} else{$estatus ="Terminado";} echo"$estatus"; ?>
+						</div>
+						<div class='col2' style="width: 27% !important;">
+							<input class='btn2' type='submit' value='ver encuestas' name='btn_submit' >
+							<input class='btn2' type='submit' value='Editar' name='btn_submit' >
+							<input class='btn2' style="background: #d27a01;" type='submit' value='Detener estudio' name='btn_submit' >
+							<input class='btn3' type='submit' value='Papelería' name='btn_submit'>
+		   			</div>
+   				</form>
+					<?php } } else {	echo "<h3>No Hay Resultados</h3>";}	mysqli_close($con); ?>
+  </div>
+</div>
+<?php include ("footer.php");?>
---
+++ sendEncuestaSociometria.php
@@ -0,0 +1,187 @@
+<?php
+session_start();
+if(empty( $_SESSION ['user'])){ header('Location:login_vacante.php');}
+  //Conexion a la db
+	include("clavesbd.php");
+	$con = mysqli_connect($servername, $username, $password, $dbname);
+	mysqli_set_charset($con,"utf8");
+	// Checa que la conexión se haya establecido correctamente
+	if (mysqli_connect_errno()) { echo "Failed to connect to MySQL: " . mysqli_connect_error();}
+$ide = $_GET['id'];
+
+$consulta = "SELECT personal_sociometria.IdPersonal, personal_sociometria.IdEmpresa, personal_sociometria.Nombre, personal_sociometria.Email, resencuesta_sociometria.IdEstatus
+FROM personal_sociometria
+JOIN resencuesta_sociometria ON personal_sociometria.IdPersonal = resencuesta_sociometria.IdResEncuesta
+where personal_sociometria.IdEmpresa = $ide AND resencuesta_sociometria.IdEstatus != 3 AND personal_sociometria.Email !='' ";
+     $resultV = mysqli_query($con, $consulta);
+     if (mysqli_num_rows($resultV) > 0) {
+       // output data of each row
+       while($rowV = mysqli_fetch_assoc($resultV)) {
+         $IdPersonal = $rowV['IdPersonal'];
+         $IdEmpresa = $rowV['IdEmpresa'];
+         $nombre = $rowV['Nombre'];
+         $Email2= $rowV['Email'];
+
+$email_to =   $Email2; //the address to which the email will be sent
+$name     =   'Matchpeople';
+$email    =   'non_replay@matchpeople.com';
+$subject  =   'SCANPAINT | ENCUESTA DE SEGURIDAD Y SALUD OCUPACIONAL';
+$MESSAGE_BODY ="
+					<!doctype html>
+					<html>
+					<head>
+					<meta charset='utf-8'>
+					<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
+					<meta name='viewport' content='width=device-width, initial-scale=1'>
+					<title></title>
+					<link href='https://matchpeople.com/css/boilerplate.css' rel='stylesheet' type='text/css'>
+					<link href='https://matchpeople.com/css/main.css' rel='stylesheet' type='text/css'>
+					<link href='https://matchpeople.com/css/texto.css' rel='stylesheet' type='text/css'>
+					<link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:700,300,300italic' rel='stylesheet' type='text/css'>
+					<link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Playfair+Display:400italic' rel='stylesheet' type='text/css'/>
+					</head>
+					<style media='screen'>
+						#cabezera{width: 201px; padding: 13px; text-align: center; color: #FFF;}
+						.wrapper{display: flex;flex-wrap: wrap; justify-content: space-around; }
+						.wrapper input{width: 98%; }
+						.noventaplus{width: 98%; }
+						.wrapper label{font-weight: 600; }
+					  .destacado{font-weight: bold; color: darkred;}
+					  input[type=password], input[type=text], input[type=email], input[type=number], textarea, select {
+					  	color: #4c4c4c;
+					    font-weight: 600;
+					  }
+					  .bgcarta{height: 270px;}
+						.bgcarta img{width: 100%;}
+					  p{font-size: 1.2em;}
+					</style>
+					<body>
+					<div class='bgcarta'>
+						<img src='https://matchpeople.com/img/cartainvita.png' alt=''>
+					</div>
+					<div id='contenido' style='padding:3em 0;'>
+					  <div class='wrapper'>
+					  		<div class='noventaplus'>
+					        <p>
+					        	Estimado(a) <span class='destacado'>$nombre,</span>
+					        </p>
+					        <p>
+						        En uno más de los esfuerzos por construir un gran lugar para trabajar, retador y promotor de la mejora y bienestar de nuestros trabajadores, SCANPAINT emprende hoy la realización de una Encuesta de Opinión en Seguridad y Salud Ocupacional. La encuesta tiene la intención de fortalecer y mejorar la campaña de Seguridad y Salud en nuestra Empresa así como el de crear conciencia en la prevención en accidentes y contagios por el COVID-19, en donde tu punto de vista es un elemento valioso para desarrollar algunas de las actividades de prevención.
+									</p><br>
+					        <p>
+										Match People, empresa de consultoría de Capital Humano procesará las encuestas y nos hará más sencilla la realización del ejercicio, su interpretación y presentación. Al final de este mensaje te enviaremos una liga para que accedas a la encuesta en la plataforma de Match People a fin de que compartas tu opinión.
+										Con la finalidad de procesar oportunamente la información te pedimos responder la Encuesta antes del <span style='color:darkred; font-weight: bold;'>04 de Agosto del 2021.</span>
+					        </p><br>
+									<p>
+					        	<span style='font-weight: bold;'> Para acceder al sistema, continúa con las siguientes instrucciones:</span>
+					        </p>
+					        <ol>
+					          <li>Da clic en la siguiente liga: <a href='http://demo.matchpeople.com/estudio_sociometria.php?id=$IdEmpresa:$IdPersonal' target='_blank' ><strong>Ir a encuesta</strong> </a></li>
+					          <li>Te aparecerá un breve mensaje de bienvenida y podrás iniciar el llenado de la encuesta.</li>
+					          <li>Al terminar la evaluación, oprime el botón <strong>TERMINAR Y ENVIAR</strong> para concluir la evaluación.</li>
+					        </ol>
+					        <p>
+					        	Si por alguna razón necesitas interrumpir la evaluación, puedes oprimir el botón <strong>GUARDAR</strong> para salvar temporalmente la información y continuar en una próxima sesión. <br>
+										<strong>Sin embargo, toma en cuenta que el sistema no la registrará como terminada hasta que envíes el cuestionario.</strong>
+					        </p>
+					        <p>
+						        Si tienes alguna duda respecto a este proceso, contacta vía correo a: <a href='mailto:ema@matchpeople.com' target='_blank'>ema@matchpeople.com</a><br>
+						        Quedamos atentos.
+					        </p>
+					  		</div>
+					    </div>
+					</div>
+					</body>
+					</html>
+					";
+$message  = $MESSAGE_BODY;
+$headers  = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "MIME-version: 1.0\n";
+$headers .= "Content-type: text/html; charset='UTF-8'\n";
+if(mail($email_to, $subject, $message, $headers)){
+		echo 'sent'; // we are sending this text to the ajax request telling it that the mail is sent..
+}else{
+		echo 'failed';// ... or this one to tell it that it wasn't sent
+}
+	}}?>
+	<!doctype html>
+	<html>
+	<head>
+	<meta charset="UTF-8">
+	<title>Matchpeople - Intranet Estudio de sociometría</title>
+	<style media="screen">
+	#customers td, #customers th { border: 1px solid #dddddd; text-align: left; padding: 8px;}
+	.adon {border-radius: 4px; text-decoration: none; padding: 5px; font-size: 14px; color: #FFF;}
+	</style>
+	<?php include("header.php");?>
+	<div id="contenido">
+		<?php include("menus/menu_vacantes.php");?>
+		<div class="titulosec" style="width: 95%;">
+			Listado de participantes
+			<div class="alinearderecha ">
+				<a href="sendEncuestaSociometria.php?id=1" class="btn5 ">Enviar Recordatorio</a>
+				<a href="papeleria_sociometria.php" class="btn5 ">Papelería</a>
+			</div>
+		</div>
+		<table style="width:95%; margin:0 auto;" id="customers">
+			<tr>
+				<th>Nomina</th>
+				<th>Nombre</th>
+				<th>Puesto</th>
+				<th>Departamento</th>
+				<th>Planta</th>
+				<th>Estatus</th>
+				<th>acciones</th>
+			</tr>
+		<?php $consulta = "SELECT personal_sociometria.IdPersonal, personal_sociometria.IdEmpresa, personal_sociometria.NumNomina,personal_sociometria.Nombre,
+		 personal_sociometria.Departamento, personal_sociometria.Puesto, personal_sociometria.Planta, personal_sociometria.Email, resencuesta_sociometria.IdEstatus
+		FROM personal_sociometria
+		 JOIN resencuesta_sociometria ON personal_sociometria.IdPersonal = resencuesta_sociometria.IdResEncuesta
+		where personal_sociometria.IdEmpresa = $ide";
+
+			 $resultV = mysqli_query($con, $consulta);
+			 if (mysqli_num_rows($resultV) > 0) {
+				 // output data of each row
+				 while($rowV = mysqli_fetch_assoc($resultV)) {
+					 $IdPersonal = $rowV['IdPersonal'];
+					 $IdEmpresa = $rowV['IdEmpresa'];
+					 $NumNomina = $rowV['NumNomina'];
+					 $Nombre = $rowV['Nombre'];
+					 $Departamento = $rowV['Departamento'];
+					 $Puesto = $rowV['Puesto'];
+					 $Planta = $rowV['Planta'];
+					 $Email= $rowV['Email'];
+					 $IdEstatus = $rowV['IdEstatus'];
+		?>
+		<tr>
+			<td><label><?php echo "$NumNomina";?></label></td>
+			<td><label><?php echo "$Nombre";?></label></td>
+			<td><label><?php echo "$Puesto";?></label></td>
+			<td><label><?php echo "$Departamento";?></label></td>
+			<td><label><?php echo "$Planta";?></label></td>
+			<td><label><?php switch ($IdEstatus) {
+						case 1:
+								echo "Sin responder";
+								break;
+						case 2:
+								echo "En progresso";
+								break;
+						case 3:
+								echo "Terminada";
+								break;}?>
+			</label></td>
+			<td>
+				<form action="editar_Personal.php?id=<?php echo "$IdPersonal";?>" enctype="multipart/form-data" method="post" onSubmit="return validacion();">
+					<a class="btn2 adon" href="estudio_sociometria.php?id=<?php echo "$IdEmpresa:$IdPersonal";?>" target="_blank"> Ver encuesta</a>
+					<input type='submit' class='btn2' name='btn_submit' value='Editar'>
+					<input type='submit' class='btn3' name='btn_submit' value='Eliminar'>
+				</form>
+			</td>
+		</tr>
+	<?php }	} else { echo "<h3 style='text-align: center;'>No Hay Resultados</h3>"; } mysqli_close($con);?>
+	</table>
+	</div>
+	<?php
+	include ("footer.php");
+?>
